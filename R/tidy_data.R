ddcbcb6e3368bfcb277371e72279249349ee954d
JK <jklai@gate.sinica.edu.tw>
2026-07-20 11:58:09 +0800

    completed: transform()

---
+++ R/tidy_data.R
@@ -0,0 +1,117 @@
+tidy_to_list <- function(data, formula, factor_levels = NULL)
+{
+    # If data is a vector
+    if (is.atomic(data) & is.null(dim(data)))
+    {
+        data <- data[stats::complete.cases(data)]
+        lst <- list(data)
+        names(lst) <- names(data)
+    }
+
+    # If data is a matrix
+    if (is.matrix(data))
+        data <- as.data.frame(data)
+
+    # If data is a data frame
+    if (is.data.frame(data))
+    {
+        if (is.null(formula) || missing(formula))
+            stop("`formula` must be specified, for example: y ~ x")
+
+        all_vars <- all.vars(formula)
+        y_name <- all_vars[1]
+        x_name <- all_vars[2]
+        if (length(all_vars) > 2)
+        {
+            warning(sprintf("Only the first independent variable (%s) is used.", x_name))
+            formula <- sprintf("%s ~ %s", y_name, x_name)
+            formula <- stats::as.formula(formula)
+        }
+
+        if (is.factor(data[[x_name]]) & is.null(factor_levels))
+            factor_levels <- levels(data[[x_name]])
+
+        df0 <- stats::model.frame(formula, data, drop.unused.levels = TRUE)
+        data <- split(df0[, 1], df0[, 2])
+    }
+
+    # If data is a list
+    # `is.null(dim(data))` is necessary as data frame is also a kind of list
+    if (is.list(data) & is.null(dim(data)))
+    {
+        data <- lapply(data, function(x) x[stats::complete.cases(x)])
+        n <- unlist(lapply(data, length), use.names = FALSE)
+
+        if (any(n < 3))
+            warning("All group sizes should be greater than 2.")
+
+        if (is.null(names(data)))
+            names(data) <- seq_along(data)
+
+        lst <- data
+    }
+
+    if (!is.null(factor_levels) & !missing(factor_levels))
+    {
+        if (all(names(lst) %in% factor_levels))
+            lst <- lst[factor_levels]
+        else
+            warning("`factor_levels` doesn't match the input data factor levels.")
+    }
+
+    return(lst)
+}
+
+
+tidy_to_dataframe <- function(data, formula)
+{
+    # If data is a list
+    # `is.null(dim(data))` is necessary as data frame is also a kind of list
+    if (is.list(data) & is.null(dim(data)))
+    {
+        data <- lapply(data, function(x) x[stats::complete.cases(x)])
+        data <- data
+        isub <- seq_along(data)
+        grp <- names(data)
+        if (is.null(grp)) grp <- isub
+        lst <- lapply(
+            isub,
+            function(i)
+            {
+                vct <- data[[i]]
+                vct <- vct[stats::complete.cases(vct)]
+                if (is.null(vct) || length(vct) == 0)
+                    df0 <- data.frame(y = NA_real_, x = grp[i])
+                else
+                    df0 <- data.frame(y = vct, x = grp[i])
+            }
+        )
+        ret <- do.call(rbind.data.frame, lst)
+        ret <- ret[stats::complete.cases(ret[["y"]]), ]
+        ret[["x"]] <- as.character(ret[["x"]])
+        attr(ret, "x_name") <- "IV"
+        attr(ret, "y_name") <- "DV"
+    }
+
+    # If data is a matrix
+    if (is.matrix(data))
+        data <- as.data.frame(data)
+
+    # If data is a data frame
+    if (is.data.frame(data))
+    {
+        if (missing(formula)) stop("Please specify the `formula`.")
+        df0 <- stats::model.frame(formula, data, drop.unused.levels = TRUE)
+        x_name <- colnames(df0)[2]
+        y_name <- colnames(df0)[1]
+        colnames(df0) <- c("y", "x")
+        df0[["x"]] <- as.character(df0[["x"]])
+        ret <- df0[stats::complete.cases(df0[["y"]]), ]
+        attr(ret, "x_name") <- x_name
+        attr(ret, "y_name") <- y_name
+    }
+
+    ret <- ret[order(ret[["x"]]), ]
+
+    return(ret)
+}
---
+++ R/transformation.R
@@ -0,0 +1,126 @@
+transform_options <- c("art", "sq", "sqrt", "log", "log2", "log10", "recip")
+
+transform <- function(data, formula, method = "art", simplify = FALSE, misc = FALSE)
+{
+    method <- match.arg(tolower(method[1]), transform_options)
+    m <- match(method, transform_options)
+
+    func <- switch(m,
+                   .transform_art,
+                   .transform_sq,
+                   .transform_sqrt,
+                   .transform_log,
+                   .transform_log2,
+                   .transform_log10,
+                   .transform_reciprocal)
+
+    if (method == "art")
+        ret <- func(data, formula, simplify, misc)
+    else
+        ret <- func(data)
+
+    return(ret)
+}
+
+
+.transform_art <- function(data, formula, simplify = FALSE, misc = FALSE)
+{
+    df0 <- tidy_to_dataframe(data, formula)
+    y_bar <- mean(df0[["y"]])
+
+    aov_mod <- stats::aov(y ~ x, df0)
+    df0[["residuals"]] <- stats::residuals(aov_mod)
+
+    yij <- split(df0[["y"]], df0[["x"]])
+    residuals <- split(df0[["residuals"]], df0[["x"]])
+    estimated_effect <- tapply(df0[["y"]], df0[["x"]], function(x) mean(x) - y_bar)
+
+    aligned_y <- lapply(names(yij),
+                        function(nm)
+                        {
+                            estimated_effect <- mean(yij[[nm]]) - y_bar
+                            aligned_y <- residuals[[nm]] + estimated_effect
+                            rounding <- abs(ceiling(log10(.Machine$double.eps) / 2))
+                            aligned_y <- round(aligned_y, rounding)
+                            return(aligned_y)
+                        })
+    aligned_y <- unlist(aligned_y, use.names = FALSE)
+
+    ranked_y <- rank(unlist(aligned_y), ties.method = "average")
+
+    if (isTRUE(simplify))
+    {
+        ret <- ranked_y
+    } else {
+        df0[["aligned_y"]] <- aligned_y
+        df0[["ranked_y"]] <- ranked_y
+        ret <- df0
+    }
+
+    if (isTRUE(misc))
+    {
+        ret <- list(
+            "data" = df0,
+            "estimated_effect" = estimated_effect
+        )
+    }
+
+    return(ret)
+}
+
+
+.transform_sq <- function(y)
+{
+    # can reduce left skewness (move the peak towards left)
+    y * y
+}
+
+
+.transform_sqrt <- function(y)
+{
+    # can reduce heteroscedasticity
+    gmin <- min(y)  # grand minimum
+    gmax <- max(y)  # grand maximum
+    y_prime <- (y - gmin) / (gmax - gmin)
+    sqrt(y_prime)
+}
+
+
+.transform_log <- function(y)
+{
+    # can reduce right skewness (move the peak towards right)
+    gmin <- min(y)  # grand minimum
+    gmax <- max(y)  # grand maximum
+    y_prime <- (y - gmin) / (gmax - gmin) + 1
+    log(y_prime)
+}
+
+
+.transform_log2 <- function(y)
+{
+    # can reduce right skewness (move the peak towards right)
+    gmin <- min(y)  # grand minimum
+    gmax <- max(y)  # grand maximum
+    y_prime <- (y - gmin) / (gmax - gmin) + 1
+    log2(y_prime)
+}
+
+
+.transform_log10 <- function(y)
+{
+    # can reduce right skewness (move the peak towards right)
+    gmin <- min(y)  # grand minimum
+    gmax <- max(y)  # grand maximum
+    y_prime <- (y - gmin) / (gmax - gmin) + 1
+    log10(y_prime)
+}
+
+
+.transform_reciprocal <- function(y)
+{
+    # can reduce heteroscedasticity and kurtosis, and also move the peak towards center
+    gmin <- min(y)  # grand minimum
+    gmax <- max(y)  # grand maximum
+    y_prime <- (y - gmin) / (gmax - gmin) + 1
+    1 / y_prime
+}
